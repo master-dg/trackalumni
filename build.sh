9a750fe7c88bce969e949fad9d18d68110f186fa
divitgokani <>
2023-03-28 14:05:41 +0530

    commit

--- build.sh
+++ build.sh
@@ -2,25 +2,25 @@
 
 set -o errexit  # exit on error
 
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-python manage.py migrate  
-celery -A TrackYourAlumni.celery worker --pool=threads -l INFO
-celery -A TrackYourAlumni beat -l INFO
+# python -m pip install --upgrade pip
+# pip install -r requirements.txt
+# python manage.py migrate  
+# celery -A TrackYourAlumni.celery worker --pool=threads -l INFO
+# celery -A TrackYourAlumni beat -l INFO
 
 
 #!/bin/bash
 
 # Open first terminal and run commands
 #!/bin/bash
-# apt-get update
-# apt-get install -y screen
+apt-get update
+apt-get install -y screen
 
-# # Terminal 1
-# screen -dmS terminal1 bash -c "pip install --upgrade pip; pip install -r requirements.txt; python manage.py migrate"
+# Terminal 1
+screen -dmS terminal1 bash -c "pip install --upgrade pip; pip install -r requirements.txt; python manage.py migrate"
 
-# # Terminal 2
-# screen -dmS terminal2 bash -c "celery -A TrackYourAlumni.celery worker --pool=threads -l INFO"
+# Terminal 2
+screen -dmS terminal2 bash -c "celery -A TrackYourAlumni.celery worker --pool=threads -l INFO"
 
-# # Terminal 3
-# screen -dmS terminal3 bash -c "celery -A TrackYourAlumni beat -l INFO"
+# Terminal 3
+screen -dmS terminal3 bash -c "celery -A TrackYourAlumni beat -l INFO"
